04819fc19df4addd2bf7a37d3446cc6e1fff0ae0
Moussa Mahamat Nassour <132145833+Moussassoss@users.noreply.github.com>
2025-09-22 20:39:11 +0200

    Update models.py

--- alx_travel_app/listings/models.py
+++ alx_travel_app/listings/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Listing(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price_per_night = models.DecimalField(max_digits=8, decimal_places=2)
+    location = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
